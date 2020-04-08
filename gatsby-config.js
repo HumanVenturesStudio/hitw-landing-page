db4befc9fafc2a753652613c72e8919a232eb3cf
Johnny Benson <johnnybenson@gmail.com>
2020-04-08 14:58:43 -0400

    Add Site Metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ require('dotenv').config({
 });
 
 module.exports = {
+  siteMetadata: {
+    title: 'Foundation',
+    titleTemplate: '%s',
+    description: 'Foundation Landing Page',
+    url: 'https://www.humanventures.co', // No trailing slash allowed!
+    image: '/images/site.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@Human_Ventures',
+  },
   plugins: [
     {
       resolve: 'gatsby-plugin-sass',
