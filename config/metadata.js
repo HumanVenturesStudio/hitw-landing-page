6dfb0fcb377bfe6c8fa68eed181bb83424e04cc0
Johnny Benson <johnnybenson@gmail.com>
2020-04-27 18:39:00 -0400

    Starter: The Coop

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Koala',
-  description: 'Koala Landing Page',
-  twitter: '@Koala',
-  keywords: ['Koala', 'Kids', 'Learning', 'Enrichment'],
+  title: 'The Coop',
+  description: 'Farm fresh eggs from coop to stoop',
+  twitter: '@TheCoop',
+  keywords: ['The Coop', 'Chickens', 'Eggs'],
   lang: 'en',
   image: {
     src: '/images/site.png', // Path to your image you placed in the 'static' folder
