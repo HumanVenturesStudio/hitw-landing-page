0403a43d9f0941fe43609afcf41fd93fca338a6d
Johnny Benson <johnnybenson@gmail.com>
2020-04-30 10:36:18 -0400

    Add fonts-local.css

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,6 +9,7 @@ import withReleaseInfo from 'common/lib/withReleaseInfo';
 import SEO from 'components/SEO';
 
 import styles from './styles.module.scss';
+import '../../../config/fonts-local.css'; // Import Config CSS to ensure build as CSS (not SCSS)
 import '../../../config/theme.css'; // Import Config CSS to ensure build as CSS (not SCSS)
 import '../../../config/styles.css'; // Import Config CSS to ensure build as CSS (not SCSS)
 
