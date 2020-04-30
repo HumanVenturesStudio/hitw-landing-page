3a7141242ca78bb1f6c8592c40df3f912347d8b8
Johnny Benson <johnnybenson@gmail.com>
2020-04-30 10:51:28 -0400

    Add help links to Google Fonts config

--- config/fonts-google.js
+++ config/fonts-google.js
@@ -1,8 +1,13 @@
 /**
  * Landing Page Fonts -- Google Fonts
  * ----------------------------------------------------------------------------
+ * Google Fonts
  * @link https://fonts.google.com/
+ *
+ * For help configuring this file
  * @link https://github.com/SirPole/google-fonts-plugin
+ *
+ * For general help about loading fonts in Gatsby
  * @link https://medium.com/@matt.readout/using-google-fonts-in-your-gatsby-js-projects-b59a3abfba15
  */
 
