d6c91234c6dae7e3b73fa66a2123a08f1a3daee4
Johnny Benson <johnnybenson@gmail.com>
2020-04-30 10:35:44 -0400

    Rename google-fonts to fonts-google

---
+++ config/fonts-google.js
@@ -0,0 +1,18 @@
+/**
+ * Landing Page Fonts -- Google Fonts
+ * ----------------------------------------------------------------------------
+ * @link https://fonts.google.com/
+ * @link https://github.com/SirPole/google-fonts-plugin
+ * @link https://medium.com/@matt.readout/using-google-fonts-in-your-gatsby-js-projects-b59a3abfba15
+ */
+
+module.exports = [
+  {
+    family: `Roboto Mono`,
+    variants: [`400`, `700`],
+  },
+  {
+    family: `Roboto`,
+    subsets: [`latin`],
+  },
+];
