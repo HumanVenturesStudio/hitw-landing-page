b39b0c2bb3baa6421a9984d97528d92e9cf9ebf3
Johnny Benson <johnnybenson@gmail.com>
2020-05-12 15:37:13 -0400

    Make it easier to find and update manifest information for progressive web app

---
+++ config/manifest.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: `Foundation`,
+  short_name: `Foundation`,
+  start_url: `/`,
+  background_color: `#000000`,
+  theme_color: `#000000`,
+  // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+  // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+  display: `standalone`,
+  icon: `static/images/site.png`, // This path is relative to the root of the site.
+};
