a9fdbd85a64318665b494a89afa2aecdd16bc956
mishcellaneous <64920235+mishcellaneous@users.noreply.github.com>
2020-05-20 13:45:25 -0500

    Update metadata

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'The Coop',
-  description: 'Farm fresh eggs from coop to stoop',
-  twitter: '@TheCoop',
-  keywords: ['The Coop', 'Chickens', 'Eggs'],
+  title: 'nu me = nutrition for women',
+  description: 'necessary nutrition for women in the first forty days',
+  twitter: '@lovenume',
+  keywords: ['postpartum', 'postnatal', 'new mom', 'new born', ],
   lang: 'en',
   image: {
     src: '/images/site.png', // Path to your image you placed in the 'static' folder
