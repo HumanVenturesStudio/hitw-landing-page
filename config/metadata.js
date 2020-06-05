84353545df2b01d21c4828c0d71290d67bb8dee1
Johnny Benson <johnnybenson@gmail.com>
2020-06-05 12:54:51 -0400

    Nume build (first pass)

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,16 @@
 module.exports = {
-  title: 'The Coop',
-  description: 'Farm fresh eggs from coop to stoop',
-  twitter: '@TheCoop',
-  keywords: ['The Coop', 'Chickens', 'Eggs'],
+  title: 'nu me | 2020',
+  description:
+    'Healing nutrients for postpartum women, made easy. nu me is a morning powder blend, designed for better, faster postpartum recovery, based on ancient healing recipes from around the world.',
+  twitter: '@nume',
+  keywords: [
+    'nu me',
+    'nume',
+    'postpartum',
+    'recovery',
+    'nutrition',
+    'wellness',
+  ],
   lang: 'en',
   image: {
     src: '/images/site.png', // Path to your image you placed in the 'static' folder
