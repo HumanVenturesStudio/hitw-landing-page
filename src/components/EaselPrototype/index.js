dd33584efa91786aad3774e1b7f00bcedcfdd9d1
Johnny Benson <johnnybenson@gmail.com>
2020-06-24 18:15:44 -0400

    Swap easel out for the Shorty!

--- src/components/EaselPrototype/index.js
+++ src/components/EaselPrototype/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import content from './images/easel-content.gif';
-import object from './images/easel-object.png';
+import content from './images/easel-animation.gif';
 import styles from './styles.module.scss';
 
 export default function EaselPrototype() {
   return (
-    <div
-      className={styles.EaselPrototype}
-      style={{ backgroundImage: `url(${object})` }}
-    >
+    <div className={styles.EaselPrototype}>
       <img alt="Easel Content" src={content} />
-      <img alt="Easel Prototype" src={object} />
     </div>
   );
 }
