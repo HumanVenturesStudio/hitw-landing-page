52e77a8a4f84b8d32a0069fc8933e1425fe4d347
Johnny Benson <johnnybenson@gmail.com>
2020-06-24 11:22:50 -0400

    Add Support to Badgelist for 3 and 4 layout

--- src/components/BadgeList/index.js
+++ src/components/BadgeList/index.js
@@ -1,12 +1,17 @@
+import cx from 'classnames';
 import { parseEntities } from 'common/lib/strings';
 import React from 'react';
 import styles from './styles.module.scss';
 
-export default function BadgeList({ badges }) {
+export default function BadgeList({ badges, layout = 'three' }) {
   return (
     <ul className={styles.BadgeList}>
       {badges.map((badge) => (
-        <li id={badge.id} key={badge.id} className={styles.Badge}>
+        <li
+          id={badge.id}
+          key={badge.id}
+          className={cx(styles.Badge, styles[layout])}
+        >
           <i
             className={styles.BadgeIcon}
             style={{ backgroundImage: `url("${badge.icon}")` }}
